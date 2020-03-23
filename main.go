cceb71b5a64843223a3fa6012d57bb937368e770
Evan Chan <evan.chanyiksan@gmail.com>
2020-03-22 19:51:11 -0700

    Reply with DB results to HTTP requests

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
+	"time"
 )
 
 type Person struct {
@@ -21,90 +24,37 @@ type Place struct {
 }
 
 func main() {
-	// START: Uncomment below to enable http
-
-	//router := mux.NewRouter()
-	//
-	//router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-	//	// an example API handler
-	//	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-	//})
-	//
-	//srv := &http.Server{
-	//	Handler: router,
-	//	Addr:    "127.0.0.1:8000",
-	//	// Good practice: enforce timeouts for servers you create!
-	//	WriteTimeout: 15 * time.Second,
-	//	ReadTimeout:  15 * time.Second,
-	//}
-	//
-	//log.Fatal(srv.ListenAndServe())
-
-	// END: Uncomment below to enable http
-
 	// this Pings the database trying to connect, panics on error
 	db, err := sqlx.Connect("postgres", "user=postgres dbname=postgres password=password sslmode=disable")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// Query the database, storing results in a []Person (wrapped in []interface{})
-	people := []Person{}
-	db.Select(&people, "SELECT * FROM person ORDER BY first_name ASC")
-	jason, john := people[0], people[1]
+	router := mux.NewRouter()
 
-	fmt.Printf("%#v\n%#v", jason, john)
-	// Person{FirstName:"Jason", LastName:"Moiron", Email:"jmoiron@jmoiron.net"}
-	// Person{FirstName:"John", LastName:"Doe", Email:"johndoeDNE@gmail.net"}
+	// Try curl localhost:8000/people | jq
+	router.HandleFunc("/people", func(w http.ResponseWriter, r *http.Request) {
+		people := []Person{}
+		db.Select(&people, "SELECT * FROM person ORDER BY first_name ASC")
+		json.NewEncoder(w).Encode(people)
+	})
 
-	// You can also get a single result, a la QueryRow
-	jason = Person{}
-	err = db.Get(&jason, "SELECT * FROM person WHERE first_name=$1", "Jason")
-	fmt.Printf("%#v\n", jason)
-	// Person{FirstName:"Jason", LastName:"Moiron", Email:"jmoiron@jmoiron.net"}
+	// Try curl localhost:8000/people/jane.citzen@example.com | jq
+	router.HandleFunc("/people/{email}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		email := vars["email"]
+		person := Person{}
+		err = db.Get(&person, "SELECT * FROM person WHERE email=$1", email)
+		json.NewEncoder(w).Encode(person)
+	})
 
-	// if you have null fields and use SELECT *, you must use sql.Null* in your struct
-	places := []Place{}
-	err = db.Select(&places, "SELECT * FROM place ORDER BY telcode ASC")
-	if err != nil {
-		fmt.Println(err)
-		return
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
-	usa, singsing, honkers := places[0], places[1], places[2]
 
-	fmt.Printf("%#v\n%#v\n%#v\n", usa, singsing, honkers)
-	// Place{Country:"United States", City:sql.NullString{String:"New York", Valid:true}, TelCode:1}
-	// Place{Country:"Singapore", City:sql.NullString{String:"", Valid:false}, TelCode:65}
-	// Place{Country:"Hong Kong", City:sql.NullString{String:"", Valid:false}, TelCode:852}
-
-	// Loop through rows using only one struct
-	place := Place{}
-	rows, err := db.Queryx("SELECT * FROM place")
-	for rows.Next() {
-		err := rows.StructScan(&place)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Printf("%#v\n", place)
-	}
-	// Place{Country:"United States", City:sql.NullString{String:"New York", Valid:true}, TelCode:1}
-	// Place{Country:"Hong Kong", City:sql.NullString{String:"", Valid:false}, TelCode:852}
-	// Place{Country:"Singapore", City:sql.NullString{String:"", Valid:false}, TelCode:65}
-
-	// Named queries, using `:name` as the bindvar.  Automatic bindvar support
-	// which takes into account the dbtype based on the driverName on sqlx.Open/Connect
-	_, err = db.NamedExec(`INSERT INTO person (first_name,last_name,email) VALUES (:first,:last,:email)`,
-		map[string]interface{}{
-			"first": "Bin",
-			"last":  "Smuth",
-			"email": "bensmith@allblacks.nz",
-		})
-
-	// Selects Mr. Smith from the database
-	rows, err = db.NamedQuery(`SELECT * FROM person WHERE first_name=:fn`, map[string]interface{}{"fn": "Bin"})
-
-	// Named queries can also use structs.  Their bind names follow the same rules
-	// as the name -> db mapping, so struct fields are lowercased and the `db` tag
-	// is taken into consideration.
-	rows, err = db.NamedQuery(`SELECT * FROM person WHERE first_name=:first_name`, jason)
+	log.Fatal(srv.ListenAndServe())
 }
